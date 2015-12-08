fed23f65183ffb130abe665c084dd79e8df37953
Rohit Nandwate <rohitnandwate@gmail.com>
2015-12-07 21:53:42 -0500

    MUCH faster N queens

--- src/com/rhn/algorithms/NQueens.java
+++ src/com/rhn/algorithms/NQueens.java
@@ -4,43 +4,41 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/n-queens/
+ * @author rohitnandwate
+ *
+ */
 public class NQueens {
-	public static long calls = 0;
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<List<String>>();
         int[] avail = new int[n];
         Arrays.fill(avail, -1);
         long start = System.currentTimeMillis();
         solve(n, ans, avail, 0);
-        System.out.println("Time: "+(System.currentTimeMillis()-start) +" ms");
+        System.out.println("Input n= "+n+" Time: "+(System.currentTimeMillis()-start) +" ms. Solutions: "+ans.size());
         return ans;
     }
 
 	private void solve(int n, List<List<String>> ans, int[] avail, int k) {
-		calls++;
 		if(k==n) {
 			addAns(n, ans, avail);
 			return;
 		}
 		for(int i=0; i<n; ++i) {
-			if(avail[i] != -1) {
-				continue;
+			avail[k] = i;
+			if(isValid(avail, k)) {
+				solve(n, ans, avail, k+1);
 			}
-			if(k > 0) {
-				if(i == 0 & n > 1 && avail[i+1] == k-1) {
-					continue;
-				}
-				if(i > 0 && i < n-1 && (avail[i+1] == k-1 || avail[i-1] == k-1)) {
-					continue;
-				}
-				if(i == n-1 && avail[i-1] == k-1) {
-					continue;
-				}
-			}
-			avail[i] = k;
-			solve(n, ans, avail, k+1);
-			avail[i] = -1;
 		}
+	}
+
+	private boolean isValid(int[] avail, int k) {
+		for(int i=0; i<k; ++i) {
+			if(avail[i] == avail[k] || Math.abs(avail[i]-avail[k]) == Math.abs(i-k))
+				return false;
+		}
+		return true;
 	}
 
 	private void addAns(int n, List<List<String>> ans, int[] avail) {
--- test/com/rhn/algorithms/NQueensTest.java
+++ test/com/rhn/algorithms/NQueensTest.java
@@ -4,23 +4,13 @@ import java.util.List;
 
 import org.junit.Test;
 
-import com.rhn.algorithms.NQueens.Solution;
-
 public class NQueensTest {
 
 	private NQueens nQueens = new NQueens();
-	private NQueens.Solution sol = nQueens.new Solution();
 	
 	@Test
 	public void test() {
-		List<List<String>> ans = nQueens.solveNQueens(10);
-//		nQueens.print(ans);
-		
-//		long start = System.currentTimeMillis();
-//		List<List<String>> ans = sol.solveNQueens(11);
-//		System.out.println("Time: "+(System.currentTimeMillis()-start) +" ms");
-//		nQueens.print(ans);
-		System.out.println(NQueens.calls);
+		nQueens.solveNQueens(5);
 	}
 
 }
